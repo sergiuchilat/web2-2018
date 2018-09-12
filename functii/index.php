83340d63e46b29a70e5979afd57c5bc3a00abb44
usarb <chilatsergiu@gmail.com>
2018-09-12 11:17:22 +0300

    Add files via upload
    Functii cu valori implicite

---
+++ functii/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<? 
+	function suma($x = 0, $y = 0){
+		return $x + $y;
+	}
+
+	echo suma(2, 3);
+	echo suma(4);
+	echo suma();
+
+	?>
+</body>
+</html>
